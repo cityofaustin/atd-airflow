e4bb490cdbc3b12b8fd42bce34deb1120c4a8e6c
Sergio Garcia <sergiogcx@gmail.com>
2019-11-27 15:26:49 -0600

    Restore hasura import staging

--- dags/atd_visionzero_hasura_import_staging.py
+++ dags/atd_visionzero_hasura_import_staging.py
@@ -6,12 +6,12 @@ from airflow.operators.docker_operator import DockerOperator
 
 default_args = {
         'owner'                 : 'airflow',
-        'description'           : 'Creates a new CRIS data extract (staging).',
+        'description'           : 'Imports raw CSV extracts into VZD via Hasura (staging).',
         'depend_on_past'        : False,
         'start_date'            : datetime(2019, 1, 1),
         'email_on_failure'      : False,
         'email_on_retry'        : False,
-        'retries'               : 0,
+        'retries'               : 1,
         'retry_delay'           : timedelta(minutes=5)
 }
 
@@ -19,18 +19,18 @@ default_args = {
 atd_visionzero_cris_envvars=Variable.get("atd_visionzero_cris_staging", deserialize_json=True)
 atd_visionzero_cris_volumes=Variable.get("atd_visionzero_cris_volumes", deserialize_json=True)
 
-with DAG('atd_visionzero_cris_request_staging', default_args=default_args, schedule_interval="0 3 * * *", catchup=False) as dag:
+with DAG('atd_visionzero_hasura_import_staging', default_args=default_args, schedule_interval="0 3 * * *", catchup=False) as dag:
 
         #
-        # Task: docker_command_crashes
-        # Description: Reads crash records CSV files and inserts to database via GraphQL/Hasura.
+        # Task: docker_command
+        # Description: Runs a docker container with CentOS, and waits 30 seconds before being terminated.
         #
         t1 = DockerOperator(
-                task_id='docker_command_crashes',
+                task_id='docker_command',
                 image='atddocker/atd-vz-etl:master',
                 api_version='auto',
                 auto_remove=True,
-                command="/app/process_hasura_import.py crash",
+                command="/app/process_hasura_import.py",
                 docker_url="tcp://localhost:2376",
                 network_mode="bridge",
                 environment=atd_visionzero_cris_envvars,
@@ -40,86 +40,13 @@ with DAG('atd_visionzero_cris_request_staging', default_args=default_args, sched
                 ],
         )
 
-        # #
-        # # Task: docker_command_units
-        # # Description: Reads unit records CSV files and inserts to database via GraphQL/Hasura.
-        # #
-        # t2 = DockerOperator(
-        #         task_id='docker_command_units',
-        #         image='atddocker/atd-vz-etl:master',
-        #         api_version='auto',
-        #         auto_remove=True,
-        #         command="/app/process_hasura_import.py unit",
-        #         docker_url="tcp://localhost:2376",
-        #         network_mode="bridge",
-        #         environment=atd_visionzero_cris_envvars
-        # )
         #
-        # #
-        # # Task: docker_command_units
-        # # Description: Reads person records CSV files and inserts to database via GraphQL/Hasura.
-        # #
-        # t3 = DockerOperator(
-        #         task_id='docker_command_person',
-        #         image='atddocker/atd-vz-etl:master',
-        #         api_version='auto',
-        #         auto_remove=True,
-        #         command="/app/process_hasura_import.py person",
-        #         docker_url="tcp://localhost:2376",
-        #         network_mode="bridge",
-        #         environment=atd_visionzero_cris_envvars
-        # )
+        # Task: report_errors
+        # Description: Sends a message to slack to report any errors.
         #
-        # #
-        # # Task: docker_command_units
-        # # Description: Reads primary person records CSV files and inserts to database via GraphQL/Hasura.
-        # #
-        # t4 = DockerOperator(
-        #         task_id='docker_command_primary_person',
-        #         image='atddocker/atd-vz-etl:master',
-        #         api_version='auto',
-        #         auto_remove=True,
-        #         command="/app/process_hasura_import.py primaryperson",
-        #         docker_url="tcp://localhost:2376",
-        #         network_mode="bridge",
-        #         environment=atd_visionzero_cris_envvars
-        # )
-        #
-        # #
-        # # Task: docker_command_units
-        # # Description: Reads charges records CSV files and inserts to database via GraphQL/Hasura.
-        # #
-        # t5 = DockerOperator(
-        #         task_id='docker_command_charges',
-        #         image='atddocker/atd-vz-etl:master',
-        #         api_version='auto',
-        #         auto_remove=True,
-        #         command="/app/process_hasura_import.py charges",
-        #         docker_url="tcp://localhost:2376",
-        #         network_mode="bridge",
-        #         environment=atd_visionzero_cris_envvars
-        # )
-
-        #
-        # Task: upload_aws
-        # Description: Uploads CSV files to an S3 Bucket for backup
-        #
-        # t6 = BashOperator(
-        #         task_id='upload_aws',
-        #         bash_command='echo "Not Yet Implemented"'
-        # )
-
-        #
-        # Task: clean_up
-        # Description: Removes any zip, xml, csv files from the local server.
-        #
-        clean_up = BashOperator(
-                task_id='bash_clean_up',
+        t2 = BashOperator(
+                task_id='report_errors',
                 bash_command='echo "Not Yet Implemented"'
         )
 
-        #
-        # We now set up the tasks in the right order
-        #
-        #t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7
-        t1 >> clean_up
+        t1 >> t2
