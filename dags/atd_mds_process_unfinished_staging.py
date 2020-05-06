1b7e6cfd0a736411ec7768a7f40aa0b62e0fcf8c
Sergio Garcia <sergiogcx@gmail.com>
2020-05-05 22:30:02 -0500

    Adds slack integration

--- dags/atd_mds_process_unfinished_staging.py
+++ dags/atd_mds_process_unfinished_staging.py
@@ -3,6 +3,8 @@ from airflow.models import Variable
 from datetime import datetime, timedelta
 from airflow.operators.docker_operator import DockerOperator
 
+from _slack_operators import *
+
 default_args = {
     "owner": "airflow",
     "description": "Process unfinished tasks",
@@ -12,6 +14,8 @@ default_args = {
     "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
+    "on_failure_callback"   : task_fail_slack_alert,
+    "on_success_callback"   : task_success_slack_alert,
 }
 
 
