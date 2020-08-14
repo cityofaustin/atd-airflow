c58227eb499a86263ca1fdc2ca2430bdfdaf7cd0
Sergio Garcia <sergiogcx@gmail.com>
2020-08-13 22:06:22 -0500

    Recover export production from master

--- dags/atd_visionzero_socrata_export_production.py
+++ dags/atd_visionzero_socrata_export_production.py
@@ -6,10 +6,6 @@ from airflow.operators.docker_operator import DockerOperator
 
 from _slack_operators import *
 
-environment_vars_production = Variable.get("atd_visionzero_cris_production", deserialize_json=True)
-environment_vars_staging = Variable.get("atd_visionzero_cris_staging", deserialize_json=True)
-vzv_data_query_vars = Variable.get("atd_visionzero_vzv_query_production", deserialize_json=True)
-
 default_args = {
         'owner'                 : 'airflow',
         'description'           : 'Exports data from VZD into Socrata (production).',
@@ -30,50 +26,28 @@ with DAG(
         tags=["production", "visionzero"],
 ) as dag:
 
-        socrata_backup_crashes = BashOperator(
-                task_id="socrata_backup_crashes",
-                bash_command="sh ~/dags/bash_scripts/vzv_backup_socrata.sh",
-                env=vzv_data_query_vars
-        )
-
         #
         # Task: docker_command
         # Description: Runs a docker container with CentOS, and waits 30 seconds before being terminated.
         #
-        upsert_to_staging = DockerOperator(
-                task_id='upsert_to_staging',
+        t1 = DockerOperator(
+                task_id='docker_command',
                 image='atddocker/atd-vz-etl:production',
                 api_version='auto',
                 auto_remove=True,
                 command="/app/process_socrata_export.py",
                 docker_url="tcp://localhost:2376",
                 network_mode="bridge",
-                environment=environment_vars_staging
+                environment=Variable.get("atd_visionzero_cris_production", deserialize_json=True)
         )
 
         #
-        # Task: docker_command
-        # Description: Runs a docker container with CentOS, and waits 30 seconds before being terminated.
+        # Task: report_errors
+        # Description: Sends a message to slack to report any errors.
         #
-        # upsert_to_production = DockerOperator(
-        #         task_id='upsert_to_production',
-        #         image='atddocker/atd-vz-etl:production',
-        #         api_version='auto',
-        #         auto_remove=True,
-        #         command="/app/process_socrata_export.py",
-        #         docker_url="tcp://localhost:2376",
-        #         network_mode="bridge",
-        #         environment=environment_vars_production
-        # )
-
-        #
-        # Task: recover_on_error
-        # Description: We need to recover if the last task failed
-        #
-        recover_on_error = BashOperator(
+        t2 = BashOperator(
                 task_id='report_errors',
-                trigger_rule='one_failed',
-                bash_command='echo "Not Yet Implemented"',
+                bash_command='echo "Not Yet Implemented"'
         )
 
-        socrata_backup_crashes >> upsert_to_staging >> recover_on_error
+        t1 >> t2
