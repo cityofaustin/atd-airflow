65581886a97d315203902e5efb1baead74861a0d
Sergio Garcia <sergiogcx@gmail.com>
2020-08-20 22:29:26 -0500

    Resets retries to zero

--- dags/atd_visionzero_socrata_export_production.py
+++ dags/atd_visionzero_socrata_export_production.py
@@ -13,7 +13,7 @@ default_args = {
         'start_date'            : datetime(2019, 1, 1),
         'email_on_failure'      : False,
         'email_on_retry'        : False,
-        'retries'               : 1,
+        'retries'               : 0,
         'retry_delay'           : timedelta(minutes=5),
         # 'on_failure_callback'   : task_fail_slack_alert,
 }
