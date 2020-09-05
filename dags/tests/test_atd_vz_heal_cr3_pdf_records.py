371ac9011e4450496af42237e37f1549c1405e6a
Sergio Garcia <sergiogcx@gmail.com>
2020-09-05 02:08:32 -0500

    Adds more testing to new functions

--- dags/tests/test_atd_vz_heal_cr3_pdf_records.py
+++ dags/tests/test_atd_vz_heal_cr3_pdf_records.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-import json
-from unittest.mock import patch
-from .json_helper import load_file
 from python_scripts.atd_vzd_heal_cr3_pdf_records import *
+
 
 class TestVZDHealCR3:
     @classmethod
@@ -126,3 +124,106 @@ class TestVZDHealCR3:
         Tests if raises a critical error successfully.
         """
         assert delete_file(123.4567) is False
+
+    def test_process_record_fail_a(self):
+        """
+        Tests if process_record returns false when provided False
+        """
+        assert process_record(False) is False
+
+    def test_process_record_fail_b(self):
+        """
+        Tests if process_record returns false when provided None
+        """
+        assert process_record(None) is False
+
+    def test_process_record_fail_c(self):
+        """
+        Tests if process_record returns false when provided decimal
+        """
+        assert process_record(123.1455) is False
+
+    def test_process_record_fail_d(self):
+        """
+        Tests if process_record returns false when provided decimal str
+        """
+        assert process_record("123.1455") is False
+
+    def test_process_record_fail_e(self):
+        """
+        Tests if process_record returns false when provided an empty string
+        """
+        assert process_record("") is False
+
+    def test_process_record_fail_f(self):
+        """
+        Tests if process_record returns false when provided 0
+        """
+        assert process_record(0) is False
+
+    def test_process_record_success_a(self):
+        """
+        Tests if process_record returns false when provided 0
+        """
+        assert process_record(11152580)
+        assert file_exists(11152580) is False
+
+    def test_get_records_success_a(self):
+        records = get_records(10)
+        assert isinstance(records, set)
+        assert len(records) == 10
+
+    def test_get_records_success_b(self):
+        records = get_records(1)
+        assert isinstance(records, set)
+        assert len(records) == 1
+
+    def test_get_file_metadata_cloud_success(self):
+        meta = get_file_metadata_cloud(11152580)
+        assert isinstance(meta, dict)
+        assert "mime_type" in meta
+        assert "encoding" in meta
+
+    def test_get_file_metadata_cloud_fail_a(self):
+        meta = get_file_metadata_cloud(0)
+        assert meta is None
+
+    def test_get_file_metadata_cloud_fail_b(self):
+        meta = get_file_metadata_cloud("-123")
+        assert meta is None
+
+    def test_get_file_metadata_cloud_fail_c(self):
+        meta = get_file_metadata_cloud("-2312.232")
+        assert meta is None
+
+    def test_get_file_metadata_cloud_fail_d(self):
+        meta = get_file_metadata_cloud(None)
+        assert meta is None
+
+    def test_get_file_metadata_cloud_fail_e(self):
+        meta = get_file_metadata_cloud(False)
+        assert meta is None
+
+    def test_get_file_metadata_cloud_fail_f(self):
+        meta = get_file_metadata_cloud("")
+        assert meta is None
+
+    def test_update_metadata_success(self):
+        metadata = {
+            "file_size": 123456,
+            "last_update": get_timestamp(),
+            "mime_type": "application/pdf",
+            "encoding": "binary"
+        }
+        updated = update_metadata(11152580, metadata)
+        meta = get_file_metadata_cloud(11152580)
+        assert updated
+        assert isinstance(meta, dict)
+        assert "file_size" in meta
+        assert "last_update" in meta
+        assert "mime_type" in meta
+        assert "encoding" in meta
+        assert meta["file_size"] == 123456
+        assert meta["last_update"] != ""
+        assert meta["mime_type"] == "application/pdf"
+        assert meta["encoding"] == "binary"
