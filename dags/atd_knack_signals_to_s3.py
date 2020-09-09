842b3a449728b5b3da8333de43354622fdd6a6c1
John Clary <john.clary@austintexas.gov>
2020-09-09 00:05:27 -0500

    create DAG to load signals to s3

---
+++ dags/atd_knack_signals_to_s3.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.models import Variable
+from airflow.operators.docker_operator import DockerOperator
+
+default_args = {
+    "owner": "airflow",
+    "description": "Load signals (view_197) records from Knack to S3",
+    "depend_on_past": False,
+    "start_date": datetime(2020, 9, 1),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5),
+    "catchup": False,
+}
+
+docker_image = "atddocker/atd-knack-services:production"
+
+# command args
+script = "records_to_s3"
+app_name = "data-tracker"
+container = "view_197"
+env = "prod"
+
+# assemble env vars
+env_vars_socrata = Variable.get("atd_knack_socrata", deserialize_json=True)
+env_vars_aws = Variable.get("atd_knack_aws", deserialize_json=True)
+env_vars = {**env_vars_socrata, **env_vars_aws}
+# unpack knack auth
+atd_knack_auth = Variable.get("atd_knack_auth", deserialize_json=True)
+env_vars["app_id"] = atd_knack_auth[app_name][env]["app_id"]
+env_vars["api_key"] = atd_knack_auth[app_name][env]["api_key"]
+
+with DAG(
+    dag_id="atd_knack_signals_to_s3",
+    default_args=default_args,
+    schedule_interval="05 01 * * *",
+    dagrun_timeout=timedelta(minutes=60),
+    tags=["production", "knack"],
+) as dag:
+
+    date = "{{ prev_execution_date_success or '1970-01-01' }}"
+
+    t1 = DockerOperator(
+        task_id="atd_knack_signals_to_s3",
+        image=docker_image,
+        api_version="auto",
+        auto_remove=True,
+        command=f'./atd-knack-services/services/{script}.py -a {app_name} -c {container}  -e {env} -d "{date}"',  # noqa
+        docker_url="tcp://localhost:2376",
+        network_mode="bridge",
+        environment=env_vars,
+        tty=True,
+    )
+
+    t1
+
+if __name__ == "__main__":
+    dag.cli()
