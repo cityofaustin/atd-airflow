aa03f6096bec853e7a9b46e6c0bf9552a631e154
John Clary <john.clary@austintexas.gov>
2020-10-13 16:02:26 -0500

    create atd-service-bot dag

---
+++ dags/atd_service_bot_staging.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.models import Variable
+from airflow.operators.docker_operator import DockerOperator
+
+default_args = {
+    "owner": "airflow",
+    "description": "Fetch new DTS service requests and create Github issues",
+    "depend_on_past": False,
+    "start_date": datetime(2020, 9, 1),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5),
+}
+
+docker_image = "atddocker/atd-service-bot:production"
+app_name = "dts_portal"
+env = "test"
+
+# assemble env vars
+env_vars = {}
+KNACK_AUTH = Variable.get("atd_knack_auth", deserialize_json=True)
+env_vars["KNACK_API_KEY"] = KNACK_AUTH[app_name][env]["app_id"]
+env_vars["KNACK_APP_ID"] = KNACK_AUTH[app_name][env]["api_key"]
+env_vars["GH_ACCESS_TOKEN"] = Variable.get("atd_service_bot_github_token")
+env_vars["KNACK_DTS_PORTAL_SERVICE_BOT_USERNAME"] = Variable.get(
+    "KNACK_DTS_PORTAL_SERVICE_BOT_USERNAME"
+)
+env_vars["KNACK_DTS_PORTAL_SERVICE_BOT_PASSWORD"] = Variable.get(
+    "KNACK_DTS_PORTAL_SERVICE_BOT_PASSWORD"
+)
+
+with DAG(
+    dag_id="atd_service_bot",
+    default_args=default_args,
+    schedule_interval="* * * * *",
+    dagrun_timeout=timedelta(minutes=60),
+    tags=["staging", "knack", "atd-service-bot", "github"],
+    catchup=False,
+) as dag:
+
+    t1 = DockerOperator(
+        task_id="dts_sr_to_github",
+        image=docker_image,
+        api_version="auto",
+        auto_remove=True,
+        command="./atd-service-bot/intake.py",
+        docker_url="tcp://localhost:2376",
+        network_mode="bridge",
+        environment=env_vars,
+        tty=True,
+    )
+
+    t1
+
+if __name__ == "__main__":
+    dag.cli()
