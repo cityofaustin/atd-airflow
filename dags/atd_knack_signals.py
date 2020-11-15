fde08a907e029c6e7306de3899ac87f4ef861655
John Clary <john.clary@austintexas.gov>
2020-11-15 14:16:08 -0600

    use multi-task tag for signals > s3 > agol > socrata

--- dags/atd_knack_signals.py
+++ dags/atd_knack_signals.py
@@ -5,7 +5,7 @@ from airflow.operators.docker_operator import DockerOperator
 
 default_args = {
     "owner": "airflow",
-    "description": "Load signals (view_197) records from Knack to S3",
+    "description": "Load signals (view_197) records from Knack to S3 to AGOL and Socata",
     "depend_on_past": False,
     "start_date": datetime(2020, 9, 1),
     "email_on_failure": False,
@@ -17,7 +17,9 @@ default_args = {
 docker_image = "atddocker/atd-knack-services:production"
 
 # command args
-script = "records_to_s3"
+script_task_1 = "records_to_s3"
+script_task_2 = "records_to_socrata"
+script_task_3 = "records_to_agol"
 app_name = "data-tracker"
 container = "view_197"
 env = "prod"
@@ -27,6 +29,13 @@ env_vars = Variable.get("atd_knack_aws", deserialize_json=True)
 atd_knack_auth = Variable.get("atd_knack_auth", deserialize_json=True)
 env_vars["KNACK_APP_ID"] = atd_knack_auth[app_name][env]["app_id"]
 env_vars["KNACK_API_KEY"] = atd_knack_auth[app_name][env]["api_key"]
+env_vars["SOCRATA_API_KEY_ID"] = Variable.get("atd_service_bot_socrata_api_key_id")
+env_vars["SOCRATA_API_KEY_SECRET"] = Variable.get(
+    "atd_service_bot_socrata_api_key_secret"
+)
+env_vars["SOCRATA_APP_TOKEN"] = Variable.get("atd_service_bot_socrata_app_token")
+env_vars["AGOL_USERNAME"] = Variable.get("agol_username")
+env_vars["AGOL_PASSWORD"] = Variable.get("agol_password")
 
 with DAG(
     dag_id="atd_knack_signals_to_s3",
@@ -44,14 +53,38 @@ with DAG(
         image=docker_image,
         api_version="auto",
         auto_remove=True,
-        command=f'./atd-knack-services/services/{script}.py -a {app_name} -c {container}  -e {env} -d "{date_filter}"',  # noqa
+        command=f'./atd-knack-services/services/{script_task_1}.py -a {app_name} -c {container}  -e {env} -d "{date_filter}"',  # noqa
         docker_url="tcp://localhost:2376",
         network_mode="bridge",
         environment=env_vars,
         tty=True,
     )
 
-    t1
+    t2 = DockerOperator(
+        task_id="atd_knack_signals_to_socrata",
+        image=docker_image,
+        api_version="auto",
+        auto_remove=True,
+        command=f'./atd-knack-services/services/{script_task_2}.py -a {app_name} -c {container}  -e {env} -d "{date_filter}"',  # noqa
+        docker_url="tcp://localhost:2376",
+        network_mode="bridge",
+        environment=env_vars,
+        tty=True,
+    )
+
+    t3 = DockerOperator(
+        task_id="atd_knack_signals_to_agol",
+        image=docker_image,
+        api_version="auto",
+        auto_remove=True,
+        command=f'./atd-knack-services/services/{script_task_3}.py -a {app_name} -c {container}  -e {env} -d "{date_filter}"',  # noqa
+        docker_url="tcp://localhost:2376",
+        network_mode="bridge",
+        environment=env_vars,
+        tty=True,
+    )
+
+    t1 >> [t2, t3]
 
 if __name__ == "__main__":
     dag.cli()
