9acc50905fddd8295c8b437be9464fa7feaf8f47
Sergio Garcia <sergiogcx@gmail.com>
2021-07-23 16:48:29 -0400

    Comment out revel

--- dags/atd_mds_process_unfinished_production.py
+++ dags/atd_mds_process_unfinished_production.py
@@ -129,16 +129,16 @@ with DAG(
         environment=environment_vars,
     )
 
-    revel = DockerOperator(
-        task_id="process_unfinished_revel",
-        image=docker_image,
-        api_version="auto",
-        auto_remove=True,
-        command=f"./provider_runtool.py --provider 'revel' --time-min '{time_min}' --time-max '{time_max}' --incomplete-only --no-logs",
-        docker_url="tcp://localhost:2376",
-        network_mode="bridge",
-        environment=environment_vars,
-    )
+    # revel = DockerOperator(
+    #     task_id="process_unfinished_revel",
+    #     image=docker_image,
+    #     api_version="auto",
+    #     auto_remove=True,
+    #     command=f"./provider_runtool.py --provider 'revel' --time-min '{time_min}' --time-max '{time_max}' --incomplete-only --no-logs",
+    #     docker_url="tcp://localhost:2376",
+    #     network_mode="bridge",
+    #     environment=environment_vars,
+    # )
 
     scoobi = DockerOperator(
         task_id="process_unfinished_scoobi",
