4c00d83c5d11c9a150a04ec188b56afde92be9ba
John Clary <john.clary@austintexas.gov>
2022-01-02 16:16:01 -0600

    try another aproach to handling start date

--- dags/atd_parking_data_meters_txn_history.py
+++ dags/atd_parking_data_meters_txn_history.py
@@ -28,9 +28,12 @@ with DAG(
     tags=["production", "parking"],
     catchup=False,
 ) as dag:
-    # query 7 days ago to present. some transactions are not available at endpoint
-    # until a few days after they have occurred
-    start_date = "{{ (prev_execution_date_success - datetime.timedelta(days=7)).strftime('%Y-%m-%d') if prev_execution_date_success else '2021-12-01'}}"
+    # extact prev exec date
+    start_date_str = "{{ prev_execution_date_success.strftime('%Y-%m-%d') if prev_execution_date_success else '2021-12-01'}}"
+    # convert it back to a datetime obj
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+    # subtract 7 days and convert back to a string
+    start_date = (start_date - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
 
     t1 = DockerOperator(
         task_id="parking_transaction_history_to_s3",
