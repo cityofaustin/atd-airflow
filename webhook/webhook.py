f968ffd00b517495b8a83596ce866199fe99ec7f
Frank Hereford <frankhereford@users.noreply.github.com>
2023-05-21 10:56:27 -0500

    change 1pw entry name

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -12,7 +12,7 @@ VAULT_ID = os.getenv("OP_VAULT_ID")
 
 SECRETS = {
     "webhook_token": {
-        "opitem": "Airflow Webhook Token",
+        "opitem": "Airflow Webhook",
         "opfield": ".password",
         "opvault": VAULT_ID,
     },
