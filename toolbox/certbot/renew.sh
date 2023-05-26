8b6bb285749fc62eb6929ec4b97da788f962fba6
Mike <mike.dilley@gmail.com>
2023-05-26 13:51:46 -0500

    Uncommennt the certbot part

--- toolbox/certbot/renew.sh
+++ toolbox/certbot/renew.sh
@@ -18,26 +18,28 @@ AWS_SECRET_ACCESS_KEY=$(docker run -it --rm --name op \
 -e OP_CONNECT_TOKEN=$OP_API_TOKEN \
 1password/op:2 op read op://$OP_VAULT_ID/Certbot\ IAM\ Access\ Key\ and\ Secret/accessSecret)
 
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-
 # Renew
-# CERT_PATH="/usr/local/etc/haproxy/ssl/certs/"
-# cd $CERT_PATH
-# rm cert.key
-# rm cert.crt
+CERT_PATH="/usr/local/etc/haproxy/ssl/certs/"
+cd $CERT_PATH
+rm cert.key
+rm cert.crt
 
-# docker pull certbot/dns-route53:latest
+docker pull certbot/dns-route53:latest
 
 # Use the ID and secret key from the atd-data03 IAM account
-# docker run -it --rm --name certbot -e AWS_ACCESS_KEY_ID='KEY' -e AWS_SECRET_ACCESS_KEY='SECRETKEY' -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt"  certbot/dns-route53 certonly --dns-route53 -d airflow.austinmobility.io
+docker run -it --rm --name certbot \
+-e AWS_ACCESS_KEY_ID='KEY' \
+-e AWS_SECRET_ACCESS_KEY='SECRETKEY' \ 
+-v "/etc/letsencrypt:/etc/letsencrypt" \
+-v "/var/lib/letsencrypt:/var/lib/letsencrypt" \ 
+certbot/dns-route53 certonly --dns-route53 -d airflow.austinmobility.io
 
-# cp /etc/letsencrypt/live/airflow.austinmobility.io/privkey.pem /usr/local/etc/haproxy/ssl/cert.key
+cp /etc/letsencrypt/live/airflow.austinmobility.io/privkey.pem /usr/local/etc/haproxy/ssl/cert.key
 
-# cp /etc/letsencrypt/live/airflow.austinmobility.io/fullchain.pem /usr/local/etc/haproxy/ssl/cert.crt
+cp /etc/letsencrypt/live/airflow.austinmobility.io/fullchain.pem /usr/local/etc/haproxy/ssl/cert.crt
 
-# cat /etc/letsencrypt/live/airflow.austinmobility.io/cert.pem > /usr/airflow/atd-airflow/haproxy/ssl/airflow.austinmobility.io.pem
+cat /etc/letsencrypt/live/airflow.austinmobility.io/cert.pem > /usr/airflow/atd-airflow/haproxy/ssl/airflow.austinmobility.io.pem
 
-# cat /etc/letsencrypt/live/airflow.austinmobility.io/privkey.pem >> /usr/airflow/atd-airflow/haproxy/ssl/airflow.austinmobility.io.pem
+cat /etc/letsencrypt/live/airflow.austinmobility.io/privkey.pem >> /usr/airflow/atd-airflow/haproxy/ssl/airflow.austinmobility.io.pem
 
-# /restart-airflow.sh
+/restart-airflow.sh
