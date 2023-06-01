7b9926d6e28e554f30839d4cd40859bc081a2602
Mike <mike.dilley@gmail.com>
2023-06-01 17:03:57 -0500

    Use util in other two DAGs

--- dags/atd_service_bot_issues_to_socrata.py
+++ dags/atd_service_bot_issues_to_socrata.py
@@ -4,15 +4,10 @@ from datetime import datetime, timedelta
 from airflow.models import DAG
 from airflow.operators.docker_operator import DockerOperator
 
-from onepasswordconnectsdk.client import Client, new_client
-import onepasswordconnectsdk
-
 from utils.slack_operator import task_fail_slack_alert
+from utils.onepassword import load_dict
 
 DEPLOYMENT_ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
-ONEPASSWORD_CONNECT_HOST = os.getenv("OP_CONNECT")
-ONEPASSWORD_CONNECT_TOKEN = os.getenv("OP_API_TOKEN")
-VAULT_ID = os.getenv("OP_VAULT_ID")
 
 default_args = {
     "owner": "airflow",
@@ -31,42 +26,34 @@ REQUIRED_SECRETS = {
     "GITHUB_ACCESS_TOKEN": {
         "opitem": "Service Bot",
         "opfield": "shared.githubAccessToken",
-        "opvault": VAULT_ID,
     },
     "ZENHUB_ACCESS_TOKEN": {
         "opitem": "Service Bot",
         "opfield": "shared.zenhubAccessToken",
-        "opvault": VAULT_ID,
     },
     "SOCRATA_API_KEY_ID": {
         "opitem": "Service Bot",
         "opfield": "shared.socrataApiKeyID",
-        "opvault": VAULT_ID,
     },
     "SOCRATA_API_KEY_SECRET": {
         "opitem": "Service Bot",
         "opfield": "shared.socrataApiKeySecret",
-        "opvault": VAULT_ID,
     },
     "SOCRATA_APP_TOKEN": {
         "opitem": "Service Bot",
         "opfield": "shared.socrataAppToken",
-        "opvault": VAULT_ID,
     },
     "SOCRATA_RESOURCE_ID": {
         "opitem": "Service Bot",
         "opfield": f"{DEPLOYMENT_ENVIRONMENT}.socrataResourceId",
-        "opvault": VAULT_ID,
     },
     "SOCRATA_ENDPOINT": {
         "opitem": "Service Bot",
         "opfield": f"shared.socrataEndpoint",
-        "opvault": VAULT_ID,
     },
 }
 
-client: Client = new_client(ONEPASSWORD_CONNECT_HOST, ONEPASSWORD_CONNECT_TOKEN)
-env_vars = onepasswordconnectsdk.load_dict(client, REQUIRED_SECRETS)
+env_vars = load_dict(REQUIRED_SECRETS)
 
 with DAG(
     dag_id=f"atd_service_bot_github_to_socrata_{DEPLOYMENT_ENVIRONMENT}",
