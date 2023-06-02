bc410357187dbb17a1e8dfcb9bf8a645a8fecd88
Mike <mike.dilley@gmail.com>
2023-06-02 12:56:06 -0500

    Add docstrings and some info on utilities

--- dags/utils/onepassword.py
+++ dags/utils/onepassword.py
@@ -10,13 +10,22 @@ VAULT_ID = os.getenv("OP_VAULT_ID")
 client: Client = new_client(ONEPASSWORD_CONNECT_HOST, ONEPASSWORD_CONNECT_TOKEN)
 
 
-def load_dict(REQUIRED_SECRETS):
-    # Add vault ID to each secret
-    for value in REQUIRED_SECRETS.values():
+def load_dict(required_secrets):
+    """Return dict of specified keys and secrets from 1Password vault.
+
+    Keyword arguments:
+    required_secrets -- a dict of specified keys and values of location config w/o vault id
+    """
+    for value in required_secrets.values():
         value["opvault"] = VAULT_ID
 
     return onepasswordconnectsdk.load_dict(client, REQUIRED_SECRETS)
 
 
 def get_item_by_title(entry_name):
+    """Return a specific item by item title and vault id.
+
+    Keyword arguments:
+    entry_name -- string value of the 1Password entry name in the vault
+    """
     return client.get_item_by_title(entry_name, VAULT_ID)
