b9734ef2535f1d2013c205c8c9b492c1fdb6cb6a
Frank Hereford <frankhereford@users.noreply.github.com>
2023-07-12 09:58:35 -0500

    use DockerOperator in CRIS import

--- dags/vz_cris_import.py
+++ dags/vz_cris_import.py
@@ -1,39 +1,28 @@
 import os
-import docker
-from datetime import datetime
+import pendulum
 
-from airflow.decorators import dag, task
+from airflow.decorators import dag
+from airflow.operators.docker_operator import DockerOperator
 from utils.slack_operator import task_fail_slack_alert
-
-DEPLOYMENT_ENVIRONMENT = os.environ.get("ENVIRONMENT", 'development') # operating environment
 
 @dag(
     dag_id="vz-cris-import",
     description="A process which will import the VZ CRIS data into the database on a daily basis using data on the SFTP endpoint",
     schedule="0 7 * * *",
-    start_date=datetime(2023, 6, 1),
+    start_date=pendulum.datetime(2023, 1, 1, tz="America/Chicago"),
     catchup=False,
     tags=["vision-zero", "cris", "repo:atd-vz-data"],
     on_failure_callback=task_fail_slack_alert,
 )
 def cris_import():
-    @task()
-    def invoke_cris_import_via_docker():
-        client = docker.from_env()
-        docker_image = "atddocker/vz-cris-import:latest"
-        client.images.pull(docker_image)
-        logs = client.containers.run(
-            image=docker_image, 
-            environment= {
-                "ENVIRONMENT": DEPLOYMENT_ENVIRONMENT,
-                "OP_API_TOKEN": os.getenv("OP_API_TOKEN"),
-                "OP_CONNECT": os.getenv("OP_CONNECT"),
-                "OP_VAULT_ID": os.getenv("OP_VAULT_ID"),
-            },
-            auto_remove=True,
-            )
-        return logs.decode('utf-8')
-
-    invoke_cris_import_via_docker()
+    
+    DockerOperator(
+        task_id="run_cris_import",
+        environment=dict(os.environ),
+        image="atddocker/vz-cris-import:production",
+        auto_remove=True,
+        tty=True,
+        force_pull=True,
+    )
 
 cris_import()
