2180ae847064167c2602d80ac00151aa410820b1
Charlie Henry <30810522+Charlie-Henry@users.noreply.github.com>
2023-07-17 12:50:48 -0500

    requested changes

--- dags/atd_metrobike_trips.py
+++ dags/atd_metrobike_trips.py
@@ -1,16 +1,20 @@
 # Test locally with: docker compose run --rm airflow-cli dags test atd_metrobike_trips
 
+import os
+
 from datetime import datetime, timedelta
 
 from airflow.decorators import task
 from airflow.models import DAG
 from airflow.operators.docker_operator import DockerOperator
 
+from utils.onepassword import get_env_vars_task
 from utils.slack_operator import task_fail_slack_alert
+
+DEPLOYMENT_ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
 default_args = {
     "owner": "airflow",
-    "description": "Fetch metrobike trip data from dropbox and publish to Socrata",
     "depend_on_past": False,
     "start_date": datetime(2020, 12, 31),
     "email_on_failure": False,
@@ -19,8 +23,6 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
     "on_failure_callback": task_fail_slack_alert,
 }
-
-docker_image = "atddocker/atd-metrobike:production"
 
 # assemble env vars
 
@@ -43,33 +45,23 @@ REQUIRED_SECRETS = {
     },
 }
 
+# runs weekly in prod environment at 1:33am Monday
 with DAG(
     dag_id="atd_metrobike_trips",
+    description="Fetch metrobike trip data from dropbox and publish to Socrata",
     default_args=default_args,
-    schedule_interval="33 1 * * 1",  # runs weekly at 1:33am Monday
+    schedule_interval="33 1 * * 1" if DEPLOYMENT_ENVIRONMENT == "production" else None,
     dagrun_timeout=timedelta(minutes=60),
-    tags=["repo:atd-metrobike", "metrobike"],
+    tags=["repo:atd-metrobike", "metrobike", "socrata"],
     catchup=False,
 ) as dag:
-
-    @task(
-        task_id="get_env_vars",
-        execution_timeout=timedelta(seconds=30),
-    )
-    def get_env_vars():
-        from utils.onepassword import load_dict
-
-        return load_dict(REQUIRED_SECRETS)
-
-    env_vars = get_env_vars()
+    env_vars = get_env_vars_task(REQUIRED_SECRETS)
 
     t1 = DockerOperator(
         task_id="atd_metrobike_trips_socrata",
-        image=docker_image,
-        api_version="auto",
+        image="atddocker/atd-metrobike:production",
         auto_remove=True,
         command="python publish_trips.py",
-        network_mode="bridge",
         environment=env_vars,
         tty=True,
         force_pull=True,
