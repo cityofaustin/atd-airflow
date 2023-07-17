e7bd79d23d14196fd79ce7485fc3d88b230e375b
John Clary <john.clary@austintexas.gov>
2023-07-17 13:53:51 -0400

    create road conditions socrata dag

---
+++ dags/atd_road_conditions_socrata.py
@@ -0,0 +1,73 @@
+import os
+
+from airflow.models import DAG
+from airflow.operators.docker_operator import DockerOperator
+from pendulum import datetime, duration
+
+from utils.onepassword import get_env_vars_task
+from utils.knack import get_date_filter_arg
+from utils.slack_operator import task_fail_slack_alert
+
+DEPLOYMENT_ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+
+DEFAULT_ARGS = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2015, 1, 1, tz="America/Chicago"),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": duration(minutes=5),
+    "on_failure_callback": task_fail_slack_alert,
+}
+
+REQUIRED_SECRETS = {
+    "SOCRATA_API_KEY_ID": {
+        "opitem": "Socrata Key ID, Secret, and Token",
+        "opfield": "socrata.apiKeyId",
+    },
+    "SOCRATA_API_KEY_SECRET": {
+        "opitem": "Socrata Key ID, Secret, and Token",
+        "opfield": "socrata.apiKeySecret",
+    },
+    "SOCRATA_APP_TOKEN": {
+        "opitem": "Socrata Key ID, Secret, and Token",
+        "opfield": "socrata.appToken",
+    },
+    "PGREST_ENDPOINT": {
+        "opitem": "atd-road-conditions PostgREST",
+        "opfield": "production.endpoint",
+    },
+    "PGREST_JWT": {
+        "opitem": "atd-road-conditions PostgREST",
+        "opfield": "production.jwt",
+    },
+}
+
+
+with DAG(
+    dag_id="road_conditions_socrata",
+    default_args=DEFAULT_ARGS,
+    description="Fetch road condition sensor data from postgrest and publish to socrata",
+    schedule_interval="*/5 * * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
+    dagrun_timeout=duration(minutes=5),
+    tags=["repo:atd-road-conditions", "socrata"],
+    catchup=False,
+) as dag:
+
+    date_filter_arg = get_date_filter_arg(should_replace_monthly=False)
+
+    env_vars = get_env_vars_task(REQUIRED_SECRETS)
+
+    t1 = DockerOperator(
+        task_id="road_conditions_socrata",
+        image="atddocker/atd-road-conditions:production",
+        auto_remove=True,
+        command=f"./atd-road-conditions/socrata.py {date_filter_arg}",
+        environment=env_vars,
+        tty=True,
+        force_pull=True,
+        mount_tmp_dir=False,
+    )
+
+    date_filter_arg >> t1
