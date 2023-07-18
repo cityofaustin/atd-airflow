3e928c35f82d179160e05dbf2a30dd105d479c8f
Mike <mike.dilley@gmail.com>
2023-07-17 23:44:35 -0500

    Add task to get last update date, add if/else to evaluate run/not run

--- dags/vz_cr3_download.py
+++ dags/vz_cr3_download.py
@@ -3,8 +3,9 @@ import os
 from airflow.decorators import task
 from airflow.models import DAG
 from airflow.operators.docker_operator import DockerOperator
-from pendulum import datetime, duration
+from pendulum import datetime, duration, now, parse
 
+from utils.onepassword import get_env_vars_task, get_item_last_update_date
 from utils.slack_operator import task_fail_slack_alert
 
 DEPLOYMENT_ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
@@ -61,31 +62,36 @@ with DAG(
     tags=["repo:atd-vz-data", "cris", "s3", "cr3"],
     catchup=False,
 ) as dag:
+    env_vars = get_env_vars_task(REQUIRED_SECRETS)
 
-    @task(
-        task_id="get_env_vars",
-        execution_timeout=duration(seconds=30),
+    updated_at_dict = get_item_last_update_date("CRIS CR3 Download")
+
+    # 1PW returns 2023-07-11T22:32:58Z
+    @task(task_id="download_cr3", execution_timeout=duration(seconds=30))
+    def print_message(updated_at):
+        minutes_ago_utc = now() - duration(minutes=5)
+        updated_at_utc = updated_at
+
+        if updated_at_utc > minutes_ago_utc:
+            print("Download CR3")
+        else:
+            print("Don't download CR3")
+
+    t1 = print_message(updated_at=updated_at_dict["updated_at"])
+
+    t1 = DockerOperator(
+        task_id="vz_cr3_download",
+        # image=docker_image,
+        image="vz_etl",
+        api_version="auto",
+        auto_remove=True,
+        command="",
+        environment=env_vars,
+        tty=True,
+        force_pull=True,
+        mount_tmp_dir=False,
     )
-    def get_env_vars():
-        from utils.onepassword import load_dict
 
-        return load_dict(REQUIRED_SECRETS)
-
-    env_vars = get_env_vars()
-
-    print(env_vars)
-
-    # t1 = DockerOperator(
-    #     task_id="vz_cr3_download",
-    #     # image=docker_image,
-    #     image="vz_etl",
-    #     api_version="auto",
-    #     auto_remove=True,
-    #     command="",
-    #     environment=env_vars,
-    #     tty=True,
-    #     force_pull=True,
-    #     mount_tmp_dir=False,
-    # )
+    # t1
 
     # t1
