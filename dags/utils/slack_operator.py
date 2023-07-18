2d2b0d22cf605e61636f117335cfdb9edd006276
Frank Hereford <frankhereford@users.noreply.github.com>
2023-07-18 10:34:33 -0500

    use production url if in production

--- dags/utils/slack_operator.py
+++ dags/utils/slack_operator.py
@@ -1,3 +1,4 @@
+import os
 from airflow.hooks.base_hook import BaseHook
 from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 
@@ -33,6 +34,9 @@ def task_fail_slack_alert_critical(context):
 
 def task_fail_slack_alert(context):
     slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
+    log_url = context.get("task_instance").log_url
+    if os.getenv('ENVIRONMENT') == 'production':
+        log_url = log_url.replace('http://localhost:8080', 'https://airflow.austinmobility.io')
     slack_msg = """
             :red_circle: Task Failed. 
             *Task*: {task}  
@@ -44,8 +48,9 @@ def task_fail_slack_alert(context):
         dag=context.get("task_instance").dag_id,
         ti=context.get("task_instance"),
         exec_date=context.get("execution_date"),
-        log_url=context.get("task_instance").log_url,
+        log_url=log_url
     )
+
     failed_alert = SlackWebhookOperator(
         task_id="slack_failure",
         http_conn_id="slack",
