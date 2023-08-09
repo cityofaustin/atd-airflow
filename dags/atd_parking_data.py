dd332140f8624a1d35b2bd1f96664f2571b66931
Charlie Henry <30810522+Charlie-Henry@users.noreply.github.com>
2023-08-09 15:05:41 -0500

    closer, prev_month is none though

--- dags/atd_parking_data.py
+++ dags/atd_parking_data.py
@@ -136,22 +136,27 @@ REQUIRED_SECRETS = {
     },
 }
 
+
 docker_commands = [
     {
         "command": "python txn_history.py -v --report transactions",
         "task_id": "txn_history_report_transactions",
+        "dynamic_arg": "--start",
     },
     {
         "command": "python txn_history.py -v --report payments",
         "task_id": "txn_history_report_payments",
+        "dynamic_arg": "--start",
     },
     {
         "command": "python txn_history.py -v --report payments --user pard",
         "task_id": "txn_history_report_payments_user_pard",
+        "dynamic_arg": "--start",
     },
     {
         "command": "python passport_txns.py -v",
         "task_id": "passport_txns",
+        "dynamic_arg": "--start",
     },
     {
         "command": "python fiserv_email_pub.py",
@@ -160,22 +165,27 @@ docker_commands = [
     {
         "command": "python fiserv_DB.py",
         "task_id": "fiserv_DB",
+        "dynamic_arg": "--lastmonth",
     },
     {
         "command": "python payments_s3.py",
         "task_id": "payments_s3",
+        "dynamic_arg": "--lastmonth",
     },
     {
         "command": "python payments_s3.py --user pard",
         "task_id": "payments_s3_user_pard",
+        "dynamic_arg": "--lastmonth",
     },
     {
         "command": "python passport_DB.py",
         "task_id": "passport_DB",
+        "dynamic_arg": "--lastmonth",
     },
     {
         "command": "python smartfolio_s3.py",
         "task_id": "smartfolio_s3",
+        "dynamic_arg": "--lastmonth",
     },
     {
         "command": "python match_field_processing.py",
@@ -217,41 +227,16 @@ def decide_prev_month(prev_exec):
     # If in the first 8 days of the month of last few days of the month re-run
     # the previous month's data to make sure it is complete.
     if last_date.day < 8 or last_date.day > 26:
-        prev_month = True
+        return True
     else:
-        prev_month = False
+        return False
+    return False
 
 
-@task(
-    task_id="add_command_arguments",
-    multiple_outputs=True,
-)
-def add_command_arguments(commands, prev_exec, prev_month):
-    # Adding parameters to to our docker commands to include the previous exec date
-    # and wether or not to run the previous month
-
-    output_tasks = []
-    for c in commands:
-        if "txn_history.py" in c["command"]:
-            c["command"] = f"{c['command']} --start {prev_exec}"
-
-        if "passport_txns.py" in c["command"]:
-            c["command"] = f"{c['command']} --start {prev_exec}"
-
-        if "fiserv_DB.py" in c["command"]:
-            c["command"] = f"{c['command']} --lastmonth {prev_month}"
-
-        if "payments_s3.py" in c["command"]:
-            c["command"] = f"{c['command']} --lastmonth {prev_month}"
-
-        if "passport_DB.py" in c["command"]:
-            c["command"] = f"{c['command']} --lastmonth {prev_month}"
-
-        if "smartfolio_s3.py" in c["command"]:
-            c["command"] = f"{c['command']} --lastmonth {prev_month}"
-
-        output_tasks.append(c)
-    return {"docker_commands_amended": output_tasks}
+@task
+def log_commands(cmd):
+    print(cmd)
+    return
 
 
 with DAG(
@@ -259,32 +244,43 @@ with DAG(
     description="Scripts that download and process parking data for finance reporting.",
     default_args=default_args,
     schedule_interval="35 8 * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
-    dagrun_timeout=timedelta(minutes=60 * 4),
+    dagrun_timeout=timedelta(minutes=60 * 5),
     tags=["repo:atd-parking-data", "parking", "socrata", "postgrest"],
     catchup=False,
 ) as dag:
     env_vars = get_env_vars_task(REQUIRED_SECRETS)
     prev_exec = "{{ (prev_start_date_success - macros.timedelta(days=7)).strftime('%Y-%m-%d') if prev_start_date_success else '2023-08-01'}}"
-
     prev_month = decide_prev_month(prev_exec)
 
-    tasks = add_command_arguments(docker_commands, prev_exec, prev_month)
-
     docker_tasks = []
-    for docker_command in tasks["docker_commands_amended"]:
+    commands_logging = []
+
+    for docker_command in docker_commands:
+        dynamic_arg = docker_command.get("dynamic_arg") or ""
+        if dynamic_arg:
+            dynamic_arg = (
+                f"{dynamic_arg} {prev_exec}"
+                if dynamic_arg == "--start"
+                else f"{dynamic_arg} {prev_month}"
+            )
+
         docker_task = DockerOperator(
             task_id=docker_command["task_id"],
             image=docker_image,
-            command=docker_command["command"],
+            command=f"{docker_command['command']} {dynamic_arg}",
             api_version="auto",
             auto_remove=True,
             environment=env_vars,
             dag=dag,
             tty=True,
             force_pull=False,
+            retries=3,
+            retry_delay=timedelta(seconds=60),
         )
         docker_tasks.append(docker_task)
+        commands_logging.append(f"{docker_command['command']} {dynamic_arg}")
 
+    log_commands(commands_logging)
     # Set up the dependencies between tasks
     for i in range(1, len(docker_tasks)):
         docker_tasks[i - 1] >> docker_tasks[i]
