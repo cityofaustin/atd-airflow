71272bc99185f23bdfef66bea1b047bea295bd39
John Clary <john.clary@austintexas.gov>
2023-08-16 22:32:02 -0400

    remove dupe schedule from failed merge resolution

--- dags/atd_knack_dms.py
+++ dags/atd_knack_dms.py
@@ -65,7 +65,6 @@ with DAG(
     dag_id="atd_knack_dms",
     default_args=DEFAULT_ARGS,
     description="Load dms (view_1564) records from Knack to Postgrest to AGOL and Socrata",
-    schedule_interval="10 10 * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
     schedule_interval="24 * * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
     tags=["repo:atd-knack-services", "knack", "socrata", "agol", "data-tracker"],
     catchup=False,
