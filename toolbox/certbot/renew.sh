e36302f1a7d1ddd33930e7ce807443d06aae5756
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-17 14:56:22 -0500

    no build, plain stdout progress, add date and break

--- toolbox/certbot/renew.sh
+++ toolbox/certbot/renew.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+echo ""
+echo "$(date '+%Y-%m-%d %H:%M:%S')"
 echo "Renewing the certificates for the Airflow stack"
+
+
 /usr/airflow/atd-airflow/toolbox/certbot/renew_domain_with_certbot.sh airflow.austinmobility.io
 /usr/airflow/atd-airflow/toolbox/certbot/renew_domain_with_certbot.sh airflow-workers.austinmobility.io
 
-# Build the HAProxy image with the renewed certificates
 cd /usr/airflow/atd-airflow
-docker compose build haproxy
 
 # Restart the HAProxy stack to use renewed certificates
-docker compose restart haproxy
+BUILDKIT_PROGRESS=plain docker compose restart haproxy
