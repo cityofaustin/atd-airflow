c139a7d7f27902f5595d48e80bd8fb92adea94a1
John Clary <john.clary@austintexas.gov>
2024-01-02 14:52:02 -0500

    increase DAG timeouts

--- dags/atd_knack_secondary_signals.py
+++ dags/atd_knack_secondary_signals.py
@@ -18,7 +18,7 @@ DEFAULT_ARGS = {
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 0,
-    "execution_timeout": duration(minutes=5),
+    "execution_timeout": duration(minutes=30),
     "on_failure_callback": task_fail_slack_alert,
 }
 
