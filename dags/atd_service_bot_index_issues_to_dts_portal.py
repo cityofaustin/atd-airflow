78b5fbe18d4002407b3d70bb13174d889592b075
Frank Hereford <frankhereford@users.noreply.github.com>
2024-03-18 07:50:11 -0500

    Disable schedule when run locally

--- dags/atd_service_bot_index_issues_to_dts_portal.py
+++ dags/atd_service_bot_index_issues_to_dts_portal.py
@@ -45,7 +45,7 @@ REQUIRED_SECRETS = {
 with DAG(
     dag_id=f"atd_service_bot_issues_to_dts_portal_{DEPLOYMENT_ENVIRONMENT}",
     default_args=default_args,
-    schedule_interval="0 5 * * *",
+    schedule_interval="0 5 * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
     tags=["repo:atd-service-bot", "knack", "github"],
     catchup=False,
 ) as dag:
