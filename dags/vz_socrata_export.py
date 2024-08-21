fd0bf4f789dfbb153733323b7d4b3c0334479dc6
John Clary <john.clary@austintexas.gov>
2024-08-21 15:00:49 -0400

    update socrata export dag for new version of the script

--- dags/vz_socrata_export.py
+++ dags/vz_socrata_export.py
@@ -4,28 +4,20 @@ from pendulum import datetime, duration
 from airflow.models import DAG
 from airflow.operators.docker_operator import DockerOperator
 
-from utils.slack_operator import task_fail_slack_alert
 from utils.onepassword import get_env_vars_task
+from utils.slack_operator import task_fail_slack_alert
 
-DEPLOYMENT_ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(2015, 1, 1, tz="America/Chicago"),
-    "retries": 2,
-    "execution_timeout": duration(minutes=40),
-    "on_failure_callback": task_fail_slack_alert,
-}
+DEPLOYMENT_ENVIRONMENT = "prod" if os.getenv("ENVIRONMENT") == "production" else "dev"
 
 REQUIRED_SECRETS = {
-    "HASURA_ENDPOINT": {
-        "opitem": "Vision Zero graphql-engine Endpoints",
-        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.GraphQL Endpoint",
+    "SOCRATA_DATASET_CRASHES": {
+        "opitem": "Vision Zero Socrata Export v2",
+        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.SOCRATA_DATASET_CRASHES",
     },
-    "HASURA_ADMIN_KEY": {
-        "opitem": "Vision Zero graphql-engine Endpoints",
-        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.Admin Key",
+    "SOCRATA_DATASET_PEOPLE": {
+        "opitem": "Vision Zero Socrata Export v2",
+        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.SOCRATA_DATASET_PEOPLE",
     },
     "SOCRATA_KEY_ID": {
         "opitem": "Socrata Key ID, Secret, and Token",
@@ -39,34 +31,62 @@ REQUIRED_SECRETS = {
         "opitem": "Socrata Key ID, Secret, and Token",
         "opfield": "socrata.appToken",
     },
-    "SOCRATA_DATASET_CRASHES": {
-        "opitem": "Vision Zero Socrata Export",
-        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.SOCRATA_DATASET_CRASHES",
+    "HASURA_GRAPHQL_ENDPOINT": {
+        "opitem": "Vision Zero CRIS Import - v2",
+        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.HASURA_GRAPHQL_ENDPOINT",
     },
-    "SOCRATA_DATASET_PERSONS": {
-        "opitem": "Vision Zero Socrata Export",
-        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.SOCRATA_DATASET_PERSONS",
+    "HASURA_GRAPHQL_ADMIN_SECRET": {
+        "opitem": "Vision Zero CRIS Import - v2",
+        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.HASURA_GRAPHQL_ADMIN_SECRET",
     },
 }
 
+
+docker_image = f"atddocker/vz-socrata-export:{'production' if DEPLOYMENT_ENVIRONMENT == 'prod' else 'development'}"
+
+
+DEFAULT_ARGS = {
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "execution_timeout": duration(minutes=60),
+    "on_failure_callback": task_fail_slack_alert,
+}
+
+
 with DAG(
-    dag_id=f"vz_socrata_export_{DEPLOYMENT_ENVIRONMENT}",
-    description="Exports Vision Zero crash and people datasets to Socrata from Vision Zero database.",
-    default_args=default_args,
-    schedule_interval="0 4 * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
-    tags=["repo:atd-vz-data", "socrata"],
     catchup=False,
+    dag_id="vz-socrata-export",
+    description="Exports Vision Zero crash and people datasets to Socrata from Vision Zero database.",
+    default_args=DEFAULT_ARGS,
+    schedule_interval="0 4 * * *" if DEPLOYMENT_ENVIRONMENT == "prod" else None,
+    start_date=datetime(2024, 8, 1, tz="America/Chicago"),
+    tags=["vision-zero", "cris", "repo:atd-vz-data", "socrata"],
 ) as dag:
     env_vars = get_env_vars_task(REQUIRED_SECRETS)
 
-    DockerOperator(
-        task_id="socrata_export",
-        image=f"atddocker/vz-socrata-export:{DEPLOYMENT_ENVIRONMENT}",
-        api_version="auto",
-        auto_remove=True,
-        command="python socrata_export.py",
+    socrata_export_crashes = DockerOperator(
+        task_id="socrata_export_crashes",
+        docker_conn_id="docker_default",
+        image=docker_image,
+        command=f"./socrata_export.py --crashes",
         environment=env_vars,
+        auto_remove=True,
         tty=True,
         force_pull=True,
-        mount_tmp_dir=False,
     )
+
+    socrata_export_people = DockerOperator(
+        task_id="socrata_export_people",
+        docker_conn_id="docker_default",
+        image=docker_image,
+        command=f"./socrata_export.py --people",
+        environment=env_vars,
+        auto_remove=True,
+        tty=True,
+        force_pull=True,
+        trigger_rule="all_done"  # always run this task regardless of outcome of crashes task
+    )
+
+    socrata_export_crashes >> socrata_export_people
