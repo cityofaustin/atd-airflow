5248b30ad06f68ad36ac82d4059633326fdba329
Frank Hereford <frank@frankhereford.com>
2024-12-09 21:43:55 -0600

    Use underlying user IDs from slack for mentions

--- dags/test_slack_notifier.py
+++ dags/test_slack_notifier.py
@@ -32,7 +32,7 @@ with DAG(
     tags=["slack"],
     catchup=False,
 ) as dag:
-    dag.importance = "Critical DAG, must fix ASAP. Call Bob A. and Alice B. for help."
+    dag.importance = "Critical DAG, must fix ASAP. Call Bob A. and Alice B. for help. Also, <@UMS32US1E>"
     t1 = PythonOperator(
         task_id="task_fail",
         python_callable=task_fail,
