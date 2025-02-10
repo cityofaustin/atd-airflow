e6bc100c3091f736f13cb5d02f8ee61f945dee88
Frank Hereford <frank@frankhereford.com>
2025-02-10 11:25:24 -0600

    include icon and byline in test message

--- dags/test_slack_notifier.py
+++ dags/test_slack_notifier.py
@@ -32,6 +32,10 @@ with DAG(
     tags=["slack"],
     catchup=False,
 ) as dag:
+    # @UMS32US1E is @frank, you can "Copy Member ID" from a member's profile, in the hamburger menu
+    dag.byline = "Example optional byline, which supports mentions: <@UMS32US1E>"
+    dag.icon = ":test_tube:"
+
     t1 = PythonOperator(
         task_id="task_fail",
         python_callable=task_fail,
