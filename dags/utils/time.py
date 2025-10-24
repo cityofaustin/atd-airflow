0895533270f988914043915fa3378409f952345b
Charlie Henry <thomashenry@utexas.edu>
2025-10-24 12:42:31 -0500

    open 311 DAG

--- dags/utils/time.py
+++ dags/utils/time.py
@@ -6,7 +6,7 @@ from pendulum import now, parse
     task_id="get_previous_run_date",
     multiple_outputs=True,
 )
-def get_previous_run_date(**context):
+def get_previous_run_date(fallback_date="1970-01-01", **context):
     """Task to return the last successful run date in UTC datetime format.
 
     Args:
@@ -16,9 +16,10 @@ def get_previous_run_date(**context):
     Returns:
         Dict: dict containing the last run datetime and other future formats
     """
-    last_run_datetime = context.get("prev_start_date_success") or parse("1970-01-01")
+    last_run_datetime = context.get("prev_start_date_success") or parse(fallback_date)
 
     return {
         "last_run_datetime": last_run_datetime,
-        # add other formats here
+        "last_run_datetime_iso": last_run_datetime.isoformat(),
     }
+
