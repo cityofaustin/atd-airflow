b070088ef3f1540b78a78b1b7b52dde34f479b71
John Clary <john.clary@austintexas.gov>
2025-11-17 15:43:48 -0500

    bump afd dag to 745a ct

--- dags/vz_afd_ems_import.py
+++ dags/vz_afd_ems_import.py
@@ -53,7 +53,7 @@ REQUIRED_SECRETS = {
 @dag(
     dag_id="vz-afd-ems-incident-import",
     description="A DAG which imports EMS and AFD data into the Vision Zero database.",
-    schedule="0 7 * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
+    schedule="45 7 * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
     start_date=datetime(2023, 1, 1, tz="America/Chicago"),
     catchup=False,
     tags=["repo:atd-vz-data", "vision-zero", "ems", "afd", "import"],
