7ca738cfbc92ee657c4f934e889f7dcdb01aa78c
Mike <mike.dilley@gmail.com>
2025-11-20 16:53:12 -0600

    Start DAG and add secrets to correspond with new vault

---
+++ dags/atd_moped_ecapris_funding_sync.py
@@ -0,0 +1,84 @@
+# Test locally with: docker compose run --rm airflow-cli dags test atd_moped_ecapris_status_sync
+
+from os import getenv
+
+from airflow.models import DAG
+from airflow.operators.docker_operator import DockerOperator
+from pendulum import datetime, duration
+
+from utils.onepassword import get_env_vars_task
+from utils.slack_operator import task_fail_slack_alert
+
+DEPLOYMENT_ENVIRONMENT = getenv("ENVIRONMENT", "development")
+
+DEFAULT_ARGS = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2015, 1, 1, tz="America/Chicago"),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": duration(minutes=5),
+    "on_failure_callback": task_fail_slack_alert,
+}
+
+REQUIRED_SECRETS = {
+    "SOCRATA_API_KEY_ID": {
+        "opitem": "Socrata Key ID, Secret, and Token",
+        "opfield": "socrata.apiKeyId",
+    },
+    "SOCRATA_API_KEY_SECRET": {
+        "opitem": "Socrata Key ID, Secret, and Token",
+        "opfield": "socrata.apiKeySecret",
+    },
+    "SOCRATA_TOKEN": {
+        "opitem": "Socrata Key ID, Secret, and Token",
+        "opfield": "socrata.appToken",
+    },
+    "SOCRATA_ENDPOINT": {
+        "opitem": "Socrata Key ID, Secret, and Token",
+        "opfield": "socrata.endpoint",
+    },
+    "HASURA_ENDPOINT": {
+        "opitem": "Moped Hasura Admin",
+        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.Endpoint",
+    },
+    "HASURA_ADMIN_SECRET": {
+        "opitem": "Moped Hasura Admin",
+        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.Admin Secret",
+    },
+    "FUNDING_DATASET_IDENTIFIER": {
+        "opitem": "Moped ETLs",
+        "opfield": f"{DEPLOYMENT_ENVIRONMENT}.FUNDING_DATASET_IDENTIFIER",
+    }
+}
+
+
+with DAG(
+    dag_id="atd_moped_ecapris_funding_sync",
+    description="sync eCapris funding to Moped database",
+    default_args=DEFAULT_ARGS,
+    schedule_interval=(
+        "33 8 * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None
+    ),
+    dagrun_timeout=duration(minutes=30),
+    tags=["repo:atd-moped", "moped", "ecapris"],
+    catchup=False,
+) as dag:
+    docker_image = f"atddocker/atd-moped-etl-ecapris-funding:{DEPLOYMENT_ENVIRONMENT}"
+
+    env_vars = get_env_vars_task(REQUIRED_SECRETS)
+
+    t1 = DockerOperator(
+        task_id="ecapris_funding_to_moped",
+        image=docker_image,
+        docker_conn_id="docker_default",
+        auto_remove="force",
+        command=f"python3.14 ecapris_funding_sync.py",
+        environment=env_vars,
+        tty=True,
+        force_pull=True,
+        mount_tmp_dir=False,
+    )
+
+    t1
