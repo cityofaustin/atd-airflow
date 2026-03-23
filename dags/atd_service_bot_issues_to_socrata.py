43b9aa4f82ba2a3e1ce90b8e5c57705a4c7f6f11
Frank Hereford <github@frankhereford.com>
2026-03-23 17:38:43 -0500

    Update DAG schedule intervals to conditionally set based on deployment environment

--- dags/atd_service_bot_issues_to_socrata.py
+++ dags/atd_service_bot_issues_to_socrata.py
@@ -59,7 +59,7 @@ REQUIRED_SECRETS = {
 with DAG(
     dag_id=f"atd_service_bot_github_to_socrata_{DEPLOYMENT_ENVIRONMENT}",
     default_args=default_args,
-    schedule_interval="0 22 * * *",
+    schedule_interval="0 22 * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
     tags=["repo:atd-service-bot", "socrata", "github"],
     catchup=False,
 ) as dag:
