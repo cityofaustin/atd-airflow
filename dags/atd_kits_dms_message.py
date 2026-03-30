1b5e169b54b9287c8c02e447531fd21dbaa7b565
chiaberry <analuciaby@gmail.com>
2026-03-30 13:19:33 -0500

    mangled the import

--- dags/atd_kits_dms_message.py
+++ dags/atd_kits_dms_message.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 from airflow.sdk import DAG
-from airflow.providers.docker.operators import DockerOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 from pendulum import datetime, duration
 
 from utils.onepassword import get_env_vars_task
