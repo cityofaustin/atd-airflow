cef75504b8c56268a36639283f0224dae7d51215
Charlie Henry <thomashenry@utexas.edu>
2026-04-01 12:54:45 -0500

    add dag docs

--- dags/dts_pavement_ops_reporting.py
+++ dags/dts_pavement_ops_reporting.py
@@ -7,6 +7,23 @@ from pendulum import datetime, duration
 from utils.onepassword import get_env_vars_task
 from utils.slack_operator import task_fail_slack_alert
 
+doc_md = """
+## Pavement Ops Reporting ETL
+
+This DAG retrieves reports from agileassets Pavement Management Info System (PMIS) and sends the results to a  socrata dataset.
+
+## Troubleshooting
+
+You **do not** need to be on city VPN to run this locally.
+
+This DAG is not critical that it runs successfully daily, but if it is failing continuously we should investigate the issue.
+
+Feel free to trigger this DAG manually to see if that fixes the issue. 
+
+If there are issues with the credentials you might need to request a new client ID/secret using request_client.py in the dts-pavement-ops-reporting repo 
+and update the item in 1pass.
+"""
+
 DEPLOYMENT_ENVIRONMENT = getenv("ENVIRONMENT", "development")
 
 DEFAULT_ARGS = {
@@ -62,6 +79,7 @@ REQUIRED_SECRETS = {
 with DAG(
     dag_id=f"dts_pavement_ops_reporting",
     description="Uploads reports from agileassets PMIS to socrata.",
+    doc_md=doc_md,
     default_args=DEFAULT_ARGS,
     schedule="00 5 * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
     tags=["repo:dts-pavement-ops-reporting", "socrata", "pmis", "pavement"],
