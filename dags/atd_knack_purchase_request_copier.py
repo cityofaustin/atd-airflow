8c3dd24652233b1dbf8454184c106605e4bd4738
Frank Hereford <github@frankhereford.com>
2026-04-21 12:31:45 -0500

    undo a unneeded change

--- dags/atd_knack_purchase_request_copier.py
+++ dags/atd_knack_purchase_request_copier.py
@@ -70,8 +70,8 @@ with DAG(
         command=f"./atd-knack-services/services/purchase_request_copier.py -a {app_name} -c {container}",
         environment=env_vars,
         tty=True,
+        force_pull=False, # atd_knack_signals pulls this image every 5 minutes
         mount_tmp_dir=False,
-        force_pull=False,
     )
 
     t1
