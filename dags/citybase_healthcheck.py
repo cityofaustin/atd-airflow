9bf8b295fd395250747ab159de1f3d9ae6d100de
chiaberry <analuciaby@gmail.com>
2026-06-08 16:25:32 -0500

    add chia to byline

---
+++ dags/citybase_healthcheck.py
@@ -0,0 +1,42 @@
+from os import getenv
+from airflow import DAG
+from airflow.providers.http.sensors.http import HttpSensor
+from pendulum import datetime, duration
+
+from utils.slack_operator import task_fail_slack_alert
+
+DEPLOYMENT_ENVIRONMENT = getenv("ENVIRONMENT", "development")
+
+DEFAULT_ARGS = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2026, 1, 1, tz="America/Chicago"),
+    "retries": 0,
+    "execution_timeout": duration(seconds=30),
+    # "on_failure_callback": task_fail_slack_alert,
+}
+
+doc_md = ""
+
+with DAG(
+    dag_id="citybase_postback_healthcheck",
+    description="Checks citybase postback for 200 response",
+    doc_md=doc_md,
+    default_args=DEFAULT_ARGS,
+    schedule="0 * * * *" if DEPLOYMENT_ENVIRONMENT == "production" else None,
+    tags=["repo:atd-citybase", "citybase"],
+    catchup=False,
+) as dag:
+
+    dag.byline = f"Citybase postback healthcheck failed, {slack_member_ids['Chia']}"
+
+
+    check_citybase_endpoint = HttpSensor(
+        task_id="check_citybase_postback_endpoint",
+        http_conn_id="check_citybase",
+        endpoint="citybase.austinmobility.io",
+        request_params={},
+        response_check=lambda response: response.status_code == 200,
+    )
+
+    check_citybase_endpoint
